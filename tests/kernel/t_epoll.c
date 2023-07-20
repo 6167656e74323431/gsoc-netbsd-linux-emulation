6480fee29ad4e3a13c2df17649720230dc4932f1
Theodore <theo@pta.gg>
2023-07-20 11:04:08 -0400

    add more tests

--- tests/kernel/t_epoll.c
+++ tests/kernel/t_epoll.c
@@ -36,12 +36,120 @@
 
 #include "h_macros.h"
 
+ATF_TC(create_size);
+ATF_TC_HEAD(create_size, tc)
+{
+
+	atf_tc_set_md_var(tc, "descr",
+	    "Checks that epoll_create requires a non-positive size");
+}
+ATF_TC_BODY(create_size, tc)
+{
+	ATF_REQUIRE_EQ_MSG(epoll_create(-1), -1,
+	    "epoll_create succeeded unexpectedly");
+	ATF_REQUIRE_ERRNO(EINVAL, true);
+
+	ATF_REQUIRE_EQ_MSG(epoll_create(0), -1,
+	    "epoll_create succeeded unexpectedly");
+	ATF_REQUIRE_ERRNO(EINVAL, true);
+
+	RL(epoll_create(1));
+}
+
+ATF_TC(bad_epfd);
+ATF_TC_HEAD(bad_epfd, tc)
+{
+
+	atf_tc_set_md_var(tc, "descr",
+	    "Checks that epoll_ctl detects an invalid epfd");
+}
+ATF_TC_BODY(bad_epfd, tc)
+{
+	int fd;
+	struct epoll_event event;
+
+	RL(fd = epoll_create1(0));
+	event.events = EPOLLIN;
+
+	ATF_REQUIRE_EQ_MSG(epoll_ctl(-1, EPOLL_CTL_ADD, fd, &event), -1,
+	    "epoll_ctl succeeded unexpectedly");
+	ATF_REQUIRE_ERRNO(EBADF, true);
+}
+
+ATF_TC(bad_fd);
+ATF_TC_HEAD(bad_fd, tc)
+{
+
+	atf_tc_set_md_var(tc, "descr",
+	    "Checks that epoll_ctl detects an invalid fd");
+}
+ATF_TC_BODY(bad_fd, tc)
+{
+	int epfd;
+	struct epoll_event event;
+
+	RL(epfd = epoll_create1(0));
+	event.events = EPOLLIN;
+
+	ATF_REQUIRE_EQ_MSG(epoll_ctl(epfd, EPOLL_CTL_ADD, -1, &event), -1,
+	    "epoll_ctl succeeded unexpectedly");
+	ATF_REQUIRE_ERRNO(EBADF, true);
+}
+
+ATF_TC(double_add);
+ATF_TC_HEAD(double_add, tc)
+{
+
+	atf_tc_set_md_var(tc, "descr",
+	    "Checks that epoll_ctl detects if a fd has already been added");
+}
+ATF_TC_BODY(double_add, tc)
+{
+	int epfd, fd;
+	struct epoll_event event;
+
+	RL(epfd = epoll_create1(0));
+	RL(fd = epoll_create1(0));
+	event.events = EPOLLIN;
+
+	RL(epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &event));
+
+	ATF_REQUIRE_EQ_MSG(epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &event), -1,
+	    "epoll_ctl succeeded unexpectedly");
+	ATF_REQUIRE_ERRNO(EEXIST, true);
+}
+
+ATF_TC(not_added);
+ATF_TC_HEAD(not_added, tc)
+{
+
+	atf_tc_set_md_var(tc, "descr",
+	    "Checks that epoll_ctl detects if a fd has not been added");
+}
+ATF_TC_BODY(not_added, tc)
+{
+	int epfd, fd;
+	struct epoll_event event;
+
+	RL(epfd = epoll_create1(0));
+	RL(fd = epoll_create1(0));
+	event.events = EPOLLIN;
+
+	ATF_REQUIRE_EQ_MSG(epoll_ctl(epfd, EPOLL_CTL_MOD, fd, &event), -1,
+	    "epoll_ctl succeeded unexpectedly");
+	ATF_REQUIRE_ERRNO(ENOENT, true);
+
+	ATF_REQUIRE_EQ_MSG(epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL), -1,
+	    "epoll_ctl succeeded unexpectedly");
+	ATF_REQUIRE_ERRNO(ENOENT, true);
+}
+
 ATF_TC(watching_self);
 ATF_TC_HEAD(watching_self, tc)
 {
 
 	atf_tc_set_md_var(tc, "descr",
-	    "Check that epoll disallows watching itself");
+	    "Checks that epoll disallows watching itself");
 }
 ATF_TC_BODY(watching_self, tc)
 {
@@ -79,7 +187,7 @@ ATF_TC_HEAD(watch_depth, tc)
 {
 
 	atf_tc_set_md_var(tc, "descr",
-	    "Check that epoll fails when the watch depth exceeds 5");
+	    "Checks that epoll fails when the watch depth exceeds 5");
 }
 ATF_TC_BODY(watch_depth, tc)
 {
@@ -101,9 +209,13 @@ ATF_TC_BODY(watch_depth, tc)
 
 ATF_TP_ADD_TCS(tp)
 {
-	ATF_TP_ADD_TC(tp, watching_self); 
-	ATF_TP_ADD_TC(tp, watch_loops); 
-	ATF_TP_ADD_TC(tp, watch_depth); 
+	ATF_TP_ADD_TC(tp, create_size);
+	ATF_TP_ADD_TC(tp, bad_epfd);
+	ATF_TP_ADD_TC(tp, bad_fd);
+	ATF_TP_ADD_TC(tp, not_added);
+	ATF_TP_ADD_TC(tp, watching_self);
+	ATF_TP_ADD_TC(tp, watch_loops);
+	ATF_TP_ADD_TC(tp, watch_depth);
 
 	return atf_no_error();
 }
